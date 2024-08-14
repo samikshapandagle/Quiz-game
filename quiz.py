77ad093f180525f7e6b148115baab261597c1b3c
Samiksha Pandagle <124074204+samikshapandagle@users.noreply.github.com>
2024-08-14 23:57:46 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,35 @@
+print("Welcome to my quize")
+
+playing = input ("Do you want to play?")
+
+if playing != "yes":
+    quit()
+
+print("okayy! Let's play :")
+
+answer = input ("whst does CPU stands for?")
+if answer == "central processing unit":
+    print("correct!")
+else:
+    print("incorrect!")
+answer = input ("What is the capital of France?")
+if answer == " Paris":
+    print("correct!")
+else:
+    print("incorrect!")
+answer = input ("Which planet is known as the Red Planet?")
+if answer == "Mars":
+    print("correct!")
+else:
+    print("incorrect!")
+answer = input ("What is the chemical symbol for water?")
+if answer == "H2O":
+    print("correct!")
+else:
+    print("incorrect!")
+answer = input ("Which element has the atomic number 1?")
+if answer == "Hydrogen":
+    print("correct!")
+else:
+    print("incorrect!")
+    
