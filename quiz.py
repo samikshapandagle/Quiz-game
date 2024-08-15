63c4e63be9f06bd3a97a396cbd99497a79a7b659
Samiksha Pandagle <124074204+samikshapandagle@users.noreply.github.com>
2024-08-15 19:21:56 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,8 +1,9 @@
 print("Welcome to my quize")
 
-playing = input ("Do you want to play?")
+play = input ("Do you want to play?")
+score = 0
 
-if playing != "yes":
+if play != "yes":
     quit()
 
 print("okayy! Let's play :")
@@ -10,26 +11,37 @@ print("okayy! Let's play :")
 answer = input ("whst does CPU stands for?")
 if answer == "central processing unit":
     print("correct!")
-else:
-    print("incorrect!")
-answer = input ("What is the capital of France?")
-if answer == " Paris":
-    print("correct!")
-else:
-    print("incorrect!")
-answer = input ("Which planet is known as the Red Planet?")
-if answer == "Mars":
-    print("correct!")
-else:
-    print("incorrect!")
-answer = input ("What is the chemical symbol for water?")
-if answer == "H2O":
-    print("correct!")
-else:
-    print("incorrect!")
-answer = input ("Which element has the atomic number 1?")
-if answer == "Hydrogen":
-    print("correct!")
+    score +=1
 else:
     print("incorrect!")
     
+answer = input ("What is the capital of France?")
+if answer == "Paris":
+    print("correct!")
+    score +=1
+else:
+    print("incorrect!")
+    
+answer = input ("Which planet is known as the Red Planet?")
+if answer == "Mars":
+    print("correct!")
+    score +=1
+else:
+    print("incorrect!")
+    
+answer = input ("What is the chemical symbol for water?")
+if answer == "H2O":
+    print("correct!")
+    score +=1
+else:
+    print("incorrect!")
+    
+answer = input ("Which element has the atomic number 1?")
+if answer == "Hydrogen":
+    print("correct!")
+    score +=1
+else:
+    print("incorrect!")
+    
+print("You got " +  str(score)  + " questions correct!")
+print("You got" +str((score/5) * 100)+ "%.")
